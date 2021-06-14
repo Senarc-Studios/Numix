a36da07572a796b204827ee7957548f5d8a50cbe
Benitz Original <70798458+BenitzCoding@users.noreply.github.com>
2021-06-14 17:09:24 +0800

    Revert "Clearing Unnecessary errors."
    This reverts commit 8b0fd1ac5dbbbe63fc9ee75cb69a65d9c03b69da.

--- cogs/guild_events.py
+++ cogs/guild_events.py
@@ -11,60 +11,54 @@ class Guild_Events(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_member_join(self, member):
-		try:
-			MONGO_GUILD_SETTINGS = self.db.find_one({ "_id": member.guild.id })
-			
-			if MONGO_GUILD_SETTINGS["jmstatus"] != "Enabled":
-				return
-			
-			channel = discord.utils.get(member.guild.TextChannels, id=MONGO_GUILD_SETTINGS["jm"])
-			MEMBER_COUNT_RAW = f"{len(member.guild.users)}"
-			if MEMBER_COUNT_RAW.endswith("1"):
-				MEMBER_COUNT = f"{MEMBER_COUNT_RAW}st Member"
+		MONGO_GUILD_SETTINGS = self.db.find_one({ "_id": member.guild.id })
+		
+		if MONGO_GUILD_SETTINGS["jmstatus"] != "Enabled":
+			return
+		
+		channel = discord.utils.get(member.guild.TextChannels, id=MONGO_GUILD_SETTINGS["jm"])
+		MEMBER_COUNT_RAW = f"{len(member.guild.users)}"
+		if MEMBER_COUNT_RAW.endswith("1"):
+			MEMBER_COUNT = f"{MEMBER_COUNT_RAW}st Member"
 
-			elif MEMBER_COUNT_RAW.endswith("2"):
-				MEMBER_COUNT = f"{MEMBER_COUNT_RAW}nd Member"
+		elif MEMBER_COUNT_RAW.endswith("2"):
+			MEMBER_COUNT = f"{MEMBER_COUNT_RAW}nd Member"
 
-			elif MEMBER_COUNT_RAW.endswith("3"):
-				MEMBER_COUNT = f"{MEMBER_COUNT_RAW}rd Member"
+		elif MEMBER_COUNT_RAW.endswith("3"):
+			MEMBER_COUNT = f"{MEMBER_COUNT_RAW}rd Member"
 
-			else:
-				MEMBER_COUNT = f"{MEMBER_COUNT_RAW}th Member"
+		else:
+			MEMBER_COUNT = f"{MEMBER_COUNT_RAW}th Member"
 
-			responses = [f"{member.name} Joined!", f"Welcome {member.name}!", f"Hey {member.name}!", f"New Member!"]
+		responses = [f"{member.name} Joined!", f"Welcome {member.name}!", f"Hey {member.name}!", f"New Member!"]
 
-			embed = discord.Embed(color=242424, description=f"Looks like someone new joined, Welcome {member.name}! We hope you have a nice stay! Make sure you read the rules.")
-			embed.set_author(name=random.choices(responses), icon_url=member.avatar_url)
-			embed.set_image(url="https://media.giphy.com/media/8PyTvI5EOu9LbAm8uS/giphy.gif")
-			embed.set_footer(f"{MEMBER_COUNT}", icon_url=member.guild.icon_url)
-			channel.send(embed=embed)
-		except:
-			pass
+		embed = discord.Embed(color=242424, description=f"Looks like someone new joined, Welcome {member.name}! We hope you have a nice stay! Make sure you read the rules.")
+		embed.set_author(name=random.choices(responses), icon_url=member.avatar_url)
+		embed.set_image(url="https://media.giphy.com/media/8PyTvI5EOu9LbAm8uS/giphy.gif")
+		embed.set_footer(f"{MEMBER_COUNT}", icon_url=member.guild.icon_url)
+		channel.send(embed=embed)
 
 	@commands.Cog.listener()
 	async def on_member_remove(self, member):
-		try:
-			MONGO_GUILD_SETTINGS = self.db.find_one({ "_id": member.guild.id })
-			
-			if MONGO_GUILD_SETTINGS["lmstatus"] != "Enabled":
-				return
+		MONGO_GUILD_SETTINGS = self.db.find_one({ "_id": member.guild.id })
+		
+		if MONGO_GUILD_SETTINGS["lmstatus"] != "Enabled":
+			return
 
-			channel = discord.utils.get(member.guild.TextChannels, id=MONGO_GUILD_SETTINGS["lm"])
-			MEMBER_COUNT_RAW = f"{len(member.guild.users)}"
-			if MEMBER_COUNT_RAW.endswith("1"):
-				MEMBER_COUNT = f"{MEMBER_COUNT_RAW} Member left"
-			else:
-				MEMBER_COUNT = f"{MEMBER_COUNT_RAW} Members left"
+		channel = discord.utils.get(member.guild.TextChannels, id=MONGO_GUILD_SETTINGS["lm"])
+		MEMBER_COUNT_RAW = f"{len(member.guild.users)}"
+		if MEMBER_COUNT_RAW.endswith("1"):
+			MEMBER_COUNT = f"{MEMBER_COUNT_RAW} Member left"
+		else:
+			MEMBER_COUNT = f"{MEMBER_COUNT_RAW} Members left"
 
-			responses = [f"{member.name} Left", f"Bye {member.name}", f"We lost {member.name}", f"Someone Left"]
+		responses = [f"{member.name} Left", f"Bye {member.name}", f"We lost {member.name}", f"Someone Left"]
 
-			embed = discord.Embed(color=242424, description=f"Looks like someone left us, Bye {member.name},  We hope you come back! Sad to see you leave.")
-			embed.set_author(name=random.choice(responses), icon_url=member.avatar_url)
-			embed.set_image(url="https://media.giphy.com/media/8PyTvI5EOu9LbAm8uS/giphy.gif")
-			embed.set_footer(f"{MEMBER_COUNT}", icon_url=member.guild.icon_url)
-			channel.send(embed=embed)
-		except:
-			pass
+		embed = discord.Embed(color=242424, description=f"Looks like someone left us, Bye {member.name},  We hope you come back! Sad to see you leave.")
+		embed.set_author(name=random.choice(responses), icon_url=member.avatar_url)
+		embed.set_image(url="https://media.giphy.com/media/8PyTvI5EOu9LbAm8uS/giphy.gif")
+		embed.set_footer(f"{MEMBER_COUNT}", icon_url=member.guild.icon_url)
+		channel.send(embed=embed)
 
 def setup(bot):
 	bot.add_cog(Guild_Events(bot))
