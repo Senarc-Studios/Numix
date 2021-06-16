42abd7b9824670a0d6ea26e32da41b80168ae081
Benitz Original <70798458+BenitzCoding@users.noreply.github.com>
2021-06-16 20:42:41 +0800

    Fixed IDs

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -38,8 +38,8 @@ class Leveling(commands.Cog):
 
 		await level.update_one({ "_id": message.guild.id }, { "$set": { "_id": message.guild.id, f"{message.guild.id}_XP": author_data[f'{message.guild.id}_XP'] + len(message.content), f"GLOBAL_XP": author_data[f'GLOBAL_XP'] + len(message.content), "TOTAL_XP": author_data[f'TOTAL_XP'] + len(message.content), f"{message.guild.id}_TOTAL_XP": len(message.content) } })
 
-		if author_data[f"{message.author.id}_XP"] >= int((50 * (author_data[f"{message.guild.id}_LEVEL"] ** 2)) + (50 * author_data[f"{message.guild.id}_LEVEL"])):
-			await level.update_one({ "_id": message.author.id }, { "$set": { "_id": message.author.id,  f"{message.author.id}_XP": 0, f"{message.guild.id}_LEVEL": author_data[f"{message.guild.id}_LEVEL"] + 1 }})
+		if author_data[f"{message.guild.id}_XP"] >= int((50 * (author_data[f"{message.guild.id}_LEVEL"] ** 2)) + (50 * author_data[f"{message.guild.id}_LEVEL"])):
+			await level.update_one({ "_id": message.author.id }, { "$set": { "_id": message.author.id,  f"{message.guild.id}_XP": 0, f"{message.guild.id}_LEVEL": author_data[f"{message.guild.id}_LEVEL"] + 1 }})
 			if message.guild.id != 336642139381301249:
 				if greet == None:
 					greet = f"<:confetti:854263610284441600> You've Leveled up! Now, you're in level {author_data[f'{message.author.id}_LEVEL']}!"
