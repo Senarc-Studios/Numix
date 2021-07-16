d8f0b02baece62242e7e0c2955ff4ffaf77530b8
Benitz Original <benitz@numix.xyz>
2021-07-16 15:12:56 +0800

    Fixed Unresponsive Toggle Bug.

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -14,7 +14,7 @@ class Leveling(commands.Cog):
 		self.cooldown = []
 
 	async def confirm_task(self, ctx):
-		collection = self.db1.DataBase_1.settings
+		collection = self.config.db1.DataBase_1.settings
 
 		if collection.count_documents({ "_id": int(ctx.guild.id) }) == 0:
 			return collection.insert_one({ "_id": int(ctx.guild.id) })
@@ -27,6 +27,7 @@ class Leveling(commands.Cog):
 		await self.confirm_task(message)
 		mong_col = self.config.db1.DataBase_1.settings
 		for greeting in mong_col.find({ "_id": int(message.guild.id) }):
+			toggle = greeting["level_message_toggle"]
 			greet = greeting["greeting"]
 			if "{" not in greet:
 				greet = greet
@@ -62,9 +63,11 @@ class Leveling(commands.Cog):
 			if author_data[f"{message.guild.id}_XP"] >= int((50 * (author_data[f"{message.guild.id}_LEVEL"] ** 2)) + (50 * author_data[f"{message.guild.id}_LEVEL"])):
 				await level.update_one({ "_id": message.author.id }, { "$set": { "_id": message.author.id,  f"{message.guild.id}_XP": 0, f"{message.guild.id}_LEVEL": author_data[f"{message.guild.id}_LEVEL"] + 1 }})
 				if message.guild.id != 336642139381301249:
-					if greet == None:
-						greet = f"<:confetti:854263610284441600> <@!{message.author.id}> You've Leveled up! Now, you're in level {author_data[f'{message.guild.id}_LEVEL']}!"
-					await message.channel.send(greet)
+					if toggle == "disabled":
+						greet = ""
+						if greet == None:
+							greet = f"<:confetti:854263610284441600> <@!{message.author.id}> You've Leveled up! Now, you're in level {author_data[f'{message.guild.id}_LEVEL']}!"
+						await message.channel.send(greet)
 
 			if author_data[f"GLOBAL_XP"] >= int((50 * (author_data[f"GLOBAL_LEVEL"] ** 2)) + (50 * author_data[f"GLOBAL_LEVEL"])):
 				await level.update_one({ "_id": message.author.id }, { "$set": { "_id": message.author.id, f"GLOBAL_XP": 0, f"GLOBAL_LEVEL": author_data["GLOBAL_LEVEL"] + 1 } })
