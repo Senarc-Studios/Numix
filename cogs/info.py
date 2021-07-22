19fe48855efcadd2877014566e58d7906979f063
Benitz Original <benitz@numix.xyz>
2021-07-22 20:41:27 +0800

    Found the problem, it wasn't a int, it's a string, I'm a dummy.

--- cogs/info.py
+++ cogs/info.py
@@ -101,10 +101,10 @@ class general(commands.Cog):
 			
 			for i in disc_list:
 				count = count + 1
-				
+
 			for i in disc_list:
 				
-				if i == 0 or i == 1 or i == 2 or i == 3 or i == 4 or i == 5 or i == 6 or i == 7 or i == 8 or i == 9:
+				if i == "0" or i == "1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8" or i == "9":
 					continue
 
 				else:
