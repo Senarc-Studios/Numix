1ef6218a328513dbe6bc02fa15832ea516b03128
Benitz Original <benitz@numix.xyz>
2021-08-24 08:40:22 +0800

    Working on Leaderboards

--- cogs/info.py
+++ cogs/info.py
@@ -414,9 +414,9 @@ class general(commands.Cog):
 	async def leaderboard(self, ctx, type=None):
 		if type == None:
 			rankings = leveling.find().sort(f"{ctx.guild.id}_TOTAL_XP", -1)
-			i = 1
+			i = 0
 			embed = discord.Embed(timestamp=ctx.message.created_at, title="Leaderboard:")
-			for x in rankings:
+			for x in json.load(rankings[0]):
 				try:
 					temp = ctx.guild.get_member(x["_id"])
 					tempxp = x[f"{ctx.guild.id}_TOTAL_XP"]
@@ -424,7 +424,7 @@ class general(commands.Cog):
 					i += 1
 				except:
 					pass
-				if i == 11:
+				if i == 10:
 					break
 			embed.set_footer(text="Numix", icon_url=self.config.logo)
 			await ctx.send(embed=embed)
@@ -433,7 +433,7 @@ class general(commands.Cog):
 			rankings = leveling.find().sort("TOTAL_XP", -1)
 			i = 0
 			embed = discord.Embed(timestamp=ctx.message.created_at, title="Leaderboard:")
-			for x in rankings[0]:
+			for x in json.load(rankings[0]):
 				try:
 					temp = discord.utils.get(self.bot.users, x["_id"])
 					tempxp = x[f"TOTAL_XP"]
