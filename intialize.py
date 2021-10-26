2f5ab29959f3f052c8b0b275028874b3ffc54341
Benitz Original <benitz@numix.xyz>
2021-10-26 09:02:17 +0800

    Fixed File Name

--- intialize.py
+++ intialize.py
@@ -1,10 +1,51 @@
 import os
+import sys
 os.system("pip install -U cool-utils")
 os.system("python3 -m pip install -U cool-utils")
 
 import utils
 
-os.system(utils.get_command("pip") + " install -r requirements.txt")
+def get_command(command: str):
+	PYTHON = {
+		'linux': 'python3',
+		'win32': 'python',
+		'macos': 'python3'
+	}
+
+	PIP = {
+		'linux': 'python3 -m pip',
+		'win32': 'pip',
+		'macos': 'python3 -m pip'
+	}
+
+	CLEAR = {
+		'linux': 'clear',
+		'win32': 'cls',
+		'macos': 'clear'
+	}
+
+	LS = {
+		'linux': 'ls',
+		'win32': 'dir',
+		'macos': 'ls'
+	}
+	command = command.lower()
+	if command == "python":
+		return PYTHON[sys.platform]
+
+	elif command == "pip":
+		return PIP[sys.platform]
+
+	elif command == "clear":
+		return CLEAR[sys.platform]
+
+	elif command == "ls":
+		return LS[sys.platform]
+
+	else:
+		return command
+
+os.system(get_command("pip") + " install -r requirements.txt")
 
 def main():
 	try:
